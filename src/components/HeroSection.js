2719b2a78def66a919c7835c8ff7cf23e835c5f8
James Harkins <jh.harkins@protonmail.com>
2023-01-26 18:34:12 -0700

    Docs: update golf_course.mov to golf_course.mp4 for smaller file size

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import './HeroSection.css'
 function HeroSection() {
   return (
     <div className='hero-container'>
-        <video src="/videos/golf_course_1.mov" autoPlay loop muted />
+        <video src="/videos/golf_course_1.mp4" autoPlay loop muted />
         <h1>WELCOME BACK TO THE KPC</h1>
         <p>It's the most wonderful time of the year...</p>
         <div className="hero-btns">
