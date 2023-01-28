4a4e816a6622400868b4c4b9cd2a55dd573e3096
James Harkins <jh.harkins@protonmail.com>
2023-01-27 20:32:01 -0700

    Docs: update background images for login and register pages

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -15,10 +15,10 @@ export default function LoginForm() {
         <h1>LOG IN</h1>
         <form className="login-form" onSubmit={handleSubmit}>
             <label htmlFor="email">E-mail</label>
-            <input value={email} type="email" placeholder="E-mail address" id="email" name="email"/>
+            <input defaultValue={email} type="email" placeholder="E-mail address" id="email" name="email"/>
             <label htmlFor="password">Password</label>
-            <input value={password} type="password" placeholder="********" id="password" name="password"/>
-            <button className="submit-button" type="submit">Log In</button>
+            <input defaultValue={password} type="password" placeholder="********" id="password" name="password"/>
+            <button className="submit-button" type="submit" fullWidth>Log In</button>
         </form>
         <Link className="redirect-link" to="/register">Don't have an account set up? Register here.</Link>
     </div>
--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -18,15 +18,15 @@ export default function RegisterForm() {
         <h1>REGISTER</h1>
         <form className="register-form" onSubmit={handleSubmit}>
             <label htmlFor="firstName">First name</label>
-            <input value={firstName} type="firstName" placeholder="First name" id="firstName" name="firstName"/>
+            <input defaultValue={firstName} type="firstName" placeholder="First name" id="firstName" name="firstName"/>
             <label htmlFor="lastName">Last name</label>
-            <input value={lastName} type="lastName" placeholder="Last name" id="lastName" name="lastName"/>
+            <input defaultValue={lastName} type="lastName" placeholder="Last name" id="lastName" name="lastName"/>
             <label htmlFor="email">E-mail</label>
-            <input value={email} type="email" placeholder="E-mail address" id="email" name="email"/>
+            <input defaultValue={email} type="email" placeholder="E-mail address" id="email" name="email"/>
             <label htmlFor="password">Password</label>
-            <input value={password} type="password" placeholder="********" id="password" name="password"/>
+            <input defaultValue={password} type="password" placeholder="********" id="password" name="password"/>
             <label htmlFor="passwordConfirmation">Confirm password</label>
-            <input value={passwordConfirmation} type="passwordConfirmation" placeholder="********" id="passwordConfirmation" name="passwordConfirmation"/>
+            <input defaultValue={passwordConfirmation} type="passwordConfirmation" placeholder="********" id="passwordConfirmation" name="passwordConfirmation"/>
             <button className="submit-button" type="submit">Register</button>
         </form>
         <Link className="redirect-link" to="/login">Already have an account set up? Log in here.</Link>    
