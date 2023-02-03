fe7b9f1ebb0d3ece73268c60d9f2bb83e218f971
James Harkins <jh.harkins@protonmail.com>
2023-02-03 07:41:39 -0700

    WIP: able to render one trip at a time in dashboard, need to loop through trips

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -2,10 +2,23 @@ import React from 'react'
 import '../../App.css'
 
 function Dashboard (props) {
+    const trips = [props.golfer.attributes.golfer_trips.data[0]]
+    debugger;
     return (
         <>
         <div>
             <h1>Welcome back, {props.golfer.attributes.first_name}!</h1>
+            <div>
+                {trips.map(trip => {
+                    return (
+                    <div key={trip.id}>
+                        <h3>KPC {trip.trip_number}</h3>
+                        <h3>Total Cost: ${trip.total_cost}</h3>
+                        <hr />
+                    </div>
+                    );
+                })}    
+            </div>
         </div>
         </>
     );
