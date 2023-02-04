4b86e2cf61701d6b69969fc3bedfef42000c71a0
James Harkins <jh.harkins@protonmail.com>
2023-02-04 15:45:44 -0700

    Refactor: refactor App.js to functional component instead of class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import {BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import UnauthNavbar from './components/UnauthNavbar';
@@ -9,78 +9,119 @@ import Login from './components/pages/Login';
 import Dashboard from './components/pages/Dashboard';
 import axios from 'axios';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-      isLoggedIn: false,
-      golfer: {},
-      nextTrip: {}
-     };
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const [golfer, setGolfer] = useState({});
+
+  const [nextTrip, setNextTrip] = useState({});
+
+  const handleLogin = (data) => {
+    setIsLoggedIn(true)
+    setGolfer(data)
+    getNextTrip();
   };
 
-  handleLogin = (data) => {
-    this.setState({
-      isLoggedIn: true,
-      golfer: data
-    })
-    this.getNextTrip();
+  const handleLogout = () => {
+    setIsLoggedIn(false)
+    setGolfer({});
   };
 
-  handleLogout = () => {
-    this.setState({
-    isLoggedIn: false,
-    golfer: {}
-    })
-  };
-
-  setNextTrip = (data) => {
-    this.setState({
-      nextTrip: data
-    })
-  }
-
-  getNextTrip = () => {
+  const getNextTrip = () => {
     axios.get('http://localhost:3001/api/v1/next_trip?api_key='.concat(process.env.REACT_APP_API_KEY), 
-   {withCredentials: true})    
+    {withCredentials: true})    
     .then(response => {
-      debugger;
-      this.setNextTrip(response.data)
+      setNextTrip(response.data)
     })
   }
 
-  // loginStatus = () => {
-  //   axios.get('http://localhost:3001/api/v1/logged_in?api_key='.concat(process.env.REACT_APP_API_KEY), 
-  //  {withCredentials: true})    
-  //   .then(response => {
-  //     if (response.data.logged_in) {
-  //       this.handleLogin(response)
-  //     } else {
-  //       this.handleLogout()
-  //     }
-  //   })
-  //   .catch(error => console.log('api errors:', error))
-  // };
-
-  // componentDidMount() {
-  //   this.loginStatus()
-  // };
-  
-  render() {
-    return (
-      <>
-        <Router>
-          {this.state.isLoggedIn ? <AuthNavbar handleLogout={this.handleLogout} loginStatus={this.loginStatus}/> : <UnauthNavbar/>}
-          <Routes>
-            <Route path='/' exact element={<Home loginStatus={this.loginStatus}/>}/>
-            <Route path='/register' exact element={<Register authStatus={this.props.isLoggedIn} authGolfer={this.props.golfer}/>}/>
-            <Route path='/login' exact element={<Login handleLogin={this.handleLogin} authStatus={this.props.isLoggedIn} authGolfer={this.props.golfer}/>}/>
-            <Route path='/dashboard' exact element={this.state.isLoggedIn ? <Dashboard golfer={this.state.golfer} nextTrip={this.state.nextTrip}/> : <Navigate to="/login" replace={true} />}/>
-          </Routes>
-        </Router>
-      </>
-    );
-  }
+  return (
+    <>
+      <Router>
+        {isLoggedIn ? <AuthNavbar handleLogout={handleLogout}/> : <UnauthNavbar/>}
+        <Routes>
+          <Route path='/' exact element={<Home/>}/>
+          <Route path='/register' exact element={<Register authStatus={isLoggedIn} authGolfer={golfer}/>}/>
+          <Route path='/login' exact element={<Login handleLogin={handleLogin} authStatus={isLoggedIn} authGolfer={golfer}/>}/>
+          <Route path='/dashboard' exact element={isLoggedIn ? <Dashboard golfer={golfer} nextTrip={nextTrip}/> : <Navigate to="/login" replace={true} />}/>
+        </Routes>
+      </Router>
+    </>
+  );
 }
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { 
+//       isLoggedIn: false,
+//       golfer: {},
+//       nextTrip: {}
+//      };
+//   };
+
+//   handleLogin = (data) => {
+//     this.setState({
+//       isLoggedIn: true,
+//       golfer: data
+//     })
+//     this.getNextTrip();
+//   };
+
+//   handleLogout = () => {
+//     this.setState({
+//     isLoggedIn: false,
+//     golfer: {}
+//     })
+//   };
+
+//   setNextTrip = (data) => {
+//     this.setState({
+//       nextTrip: data
+//     })
+//   }
+
+//   getNextTrip = () => {
+//     axios.get('http://localhost:3001/api/v1/next_trip?api_key='.concat(process.env.REACT_APP_API_KEY), 
+//    {withCredentials: true})    
+//     .then(response => {
+//       debugger;
+//       this.setNextTrip(response.data)
+//     })
+//   }
+
+//   // loginStatus = () => {
+//   //   axios.get('http://localhost:3001/api/v1/logged_in?api_key='.concat(process.env.REACT_APP_API_KEY), 
+//   //  {withCredentials: true})    
+//   //   .then(response => {
+//   //     if (response.data.logged_in) {
+//   //       this.handleLogin(response)
+//   //     } else {
+//   //       this.handleLogout()
+//   //     }
+//   //   })
+//   //   .catch(error => console.log('api errors:', error))
+//   // };
+
+//   // componentDidMount() {
+//   //   this.loginStatus()
+//   // };
+  
+//   render() {
+//     return (
+//       <>
+//         <Router>
+//           {this.state.isLoggedIn ? <AuthNavbar handleLogout={this.handleLogout} loginStatus={this.loginStatus}/> : <UnauthNavbar/>}
+//           <Routes>
+//             <Route path='/' exact element={<Home loginStatus={this.loginStatus}/>}/>
+//             <Route path='/register' exact element={<Register authStatus={this.props.isLoggedIn} authGolfer={this.props.golfer}/>}/>
+//             <Route path='/login' exact element={<Login handleLogin={this.handleLogin} authStatus={this.props.isLoggedIn} authGolfer={this.props.golfer}/>}/>
+//             <Route path='/dashboard' exact element={this.state.isLoggedIn ? <Dashboard golfer={this.state.golfer} nextTrip={this.state.nextTrip}/> : <Navigate to="/login" replace={true} />}/>
+//           </Routes>
+//         </Router>
+//       </>
+//     );
+//   }
+// }
 
 export default App;
