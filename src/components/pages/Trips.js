05cb6721d3df74ffa8a0d2fcd9a56ad523c4031a
James Harkins <jh.harkins@protonmail.com>
2023-02-07 15:37:07 -0700

    Feat: complete styling for dashboard for golfer not registered for the next upcoming trip

--- src/components/pages/Trips.js
+++ src/components/pages/Trips.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 
 function Trips (props) {
     const trips = props.golfer.attributes.golfer_trips.data
-    
+    console.log(props.nextTrip)
     const listTrips = trips.map((trip) =>
         <li className='trip__block' key={trip.id}>
           <TripBlock id={trip.id} tripNumber={trip.trip_number} totalCost={trip.total_cost} tripYear={trip.trip_year}/>
@@ -26,12 +26,12 @@ function Trips (props) {
           roman = (key[+digits.pop() + (i * 10)] || "") + roman;
         return Array(+digits.join("") + 1).join("M") + roman;
         }
-    // debugger;
+    
     return ( 
         <div className='trips'>
           <h1>Welcome back, {props.golfer.attributes.first_name}!</h1>
           <Link to='/register_next_trip' className='next-trip-link'>
-            Register for the next KPC 
+            Register for the next KPC
             {/* {romanize(props.nextTrip.attributes.number)}  */}
           </Link>
             <div className='trips__container'>
