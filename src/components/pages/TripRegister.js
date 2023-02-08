4447702ef91e96092a8c8cda41595ea5cdb04172
James Harkins <jh.harkins@protonmail.com>
2023-02-07 17:49:16 -0700

    WIP: rendering list of dates for nextTrip, need to create form and list courses, meals, and nights with checkboxes for each date

--- src/components/pages/TripRegister.js
+++ src/components/pages/TripRegister.js
@@ -3,11 +3,22 @@ import '../../App.css'
 import './TripRegister.css'
 
 function TripRegister (props) {
+    const tripCalendar = props.nextTrip.attributes.calendar
+    
+    const listCalendarDays = tripCalendar.map((day) =>
+        <li key={day.date}>
+            {day.date}
+        </li>
+    )
+    
     return (
         <>
         <div className='trip-register-container'>
             <h1>REGISTER FOR KPC {props.nextTrip.attributes.number}</h1>
-            <h3>Check off each night, meal, and round that you will attend, then click Submit.</h3>
+            <h3>Check off each night, round, and meal for which you will be in attendance, then click Submit.</h3>
+            <ul className='calendar-days-list'>
+                {listCalendarDays}
+            </ul>
         </div>
         </>
     );
