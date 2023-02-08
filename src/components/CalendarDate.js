cccf361850dcb3a6e464956d6f420f4b00ca30d1
James Harkins <jh.harkins@protonmail.com>
2023-02-08 10:33:55 -0700

    Refactor: resize props.day.date in CalendarDate.js

--- src/components/CalendarDate.js
+++ src/components/CalendarDate.js
@@ -7,7 +7,7 @@ function CalendarDate(props) {
 
   return (
     <>
-        <h5>{props.day.date}</h5>
+        <h2>{props.day.date}</h2>
         <CalendarDateNight night={props.day.night} updateNights={props.updateNights}/>
         <CalendarDateCourse course={props.day.course} updateCourses={props.updateCourses}/>
         <CalendarDateMeals meals={props.day.meals} updateMeals={props.updateMeals}/>        
